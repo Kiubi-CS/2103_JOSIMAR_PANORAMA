52c3a31095d385c3510cf576c18b9f1ad2295aba
Kiubi-CS <88506512+Kiubi-CS@users.noreply.github.com>
2021-08-06 00:31:01 -0300

    MOD_01

---
+++ data.js
@@ -0,0 +1,89 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_2103josi_001",
+      "name": "Panorama_2103.JOSI_001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.027839981439019112,
+        "pitch": -0.0020446570957091126,
+        "fov": 1.0421916344608082
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.538600401602638,
+          "pitch": 0.1453253275168933,
+          "title": "ESPAÇO KIDS",
+          "text": "Espaço para as crianças, com uma \"cama\" perto do espaço de trabalho para ficar de olho."
+        },
+        {
+          "yaw": 2.218146345026419,
+          "pitch": -0.037849661419492975,
+          "title": "LOGO",
+          "text": "Placa com logo da barbearia com destaque para quem entra no estabelecimento."
+        },
+        {
+          "yaw": 2.445604788438767,
+          "pitch": -0.11789055835556539,
+          "title": "NICHO 01",
+          "text": "Pilar revestido com as mesmas madeiras do móvel da bancada, com nichos para os tacos e para a serra."
+        },
+        {
+          "yaw": -3.1401489123150377,
+          "pitch": -0.03529415635137134,
+          "title": "PAREDE PRINCIPAL",
+          "text": "Parede principal para o espaço de recreação e espera. Feito com caixotes de feira e tubos de cobre."
+        },
+        {
+          "yaw": -2.3083352375386355,
+          "pitch": -0.06357566518057212,
+          "title": "NICHO 02",
+          "text": "Pilar revestido com as mesmas madeiras e estilo do móvel da bancada, com nichos para esposição de peças antigas."
+        },
+        {
+          "yaw": -2.1406752055670886,
+          "pitch": 0.2718934487471252,
+          "title": "MESAS",
+          "text": "Pequenas mesas para quem desejar beber alguma coisa ou para ficar esperando ou vendo a mesa de sinuca."
+        },
+        {
+          "yaw": -1.0392241370964719,
+          "pitch": -0.20299165883430348,
+          "title": "ADEGA",
+          "text": "Adega antiga na parede com iluminação."
+        }
+      ]
+    }
+  ],
+  "name": "21.03 - JOSIMAR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
